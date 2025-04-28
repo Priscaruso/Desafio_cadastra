93fd609db5234053e5e8e363ba10bd70aaebba3c
priscaruso <priscilacaruso@hotmail.com>
2025-04-28 19:16:41 -0300

    add: API client file

---
+++ app/api_client.py
@@ -0,0 +1,15 @@
+import requests
+from app.config import API_URL
+
+# Função para extrair dados da api
+def get_api_data():
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()  # Lança um erro se a resposta não for 200
+        content = response.json()
+        data = content["data"]
+        return data
+        
+    except requests.RequestException as e:
+        print(f"Erro ao requisitar os dados da API: {e}")
+        return []
